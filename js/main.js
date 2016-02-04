953f9aced06aadef6f9a72cf5104931d29367786
usood <upahar.sood@gmail.com>
2016-02-04 23:31:15 +0530

    Changes

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ jQuery(function($) {'use strict',
 	
 	
 	// Carousel Auto Slide Off
-	$('#event-carousel, #twitter-feed, #sponsor-carousel ').carousel({
+	$('#event-carousel, #sponsor-carousel ').carousel({
 		interval: false
 	});
 
